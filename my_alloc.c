8d243dd0f867409162a0285b550e74f01d175bc1
Simon <simon.wilhelmstatter@gmail.com>
2019-12-15 12:01:07 +0100

    size and pointer in 8 byte -- sizePointer util works fine

--- my_alloc.c
+++ my_alloc.c
@@ -19,88 +19,58 @@ typedef struct header{
 
 __uint64_t pointer_msb = 0;
 __uint64_t lower62 = ~(3 << 62);
-__uint64_t size_mask = 0b11111 << 3;
+__uint64_t lower4to9 = 0b11111000;
 
+
+//sizePointer util functions
 void *get_pointer_from_size_pointer(sizePointer pointer) {
     return (void *) (pointer_msb | (lower62 & (((__uint64_t) pointer) << 3)));
 }
 
 int get_size_from_size_pointer(sizePointer pointer) {
-    return (int) (size_mask & ((__uint64_t) pointer >> 59));
+    return ((int) (lower4to9 & ((__uint64_t) pointer >> 56))) + 8;
 }
-//TODO: test functions
+
+void set_pointer_msb(void *pointer) {
+    pointer_msb = (~lower62) & ((__uint64_t) pointer);
+}
+
+sizePointer build_size_pointer(int size, void *pointer) {
+    __uint64_t p = (__uint64_t) pointer;
+    __uint64_t size_normalized = (__uint64_t) ((size - 8) / 8);
+    p = (lower62 & p) >> 3;
+    size_normalized <<= 59;
+    return (sizePointer) (size_normalized | p);
+}
+
 //stores pointers to the lists of free header
-header *headers[256/16+1];
+header *headers[32];
 
 int header_size = 0;
 
 void init_my_alloc() {
-
     for (int i = 0; i < (256/16+1); ++i) {
         headers[i] = NULL;
     }
     header_size = sizeof(header);
-
+    /* the following lines test the sizePointer util methods
+    void *ptr = get_block_from_system();
+    set_pointer_msb(ptr);
+    for (int size = 8; size <= 256; size += 8) {
+        printf("ptr: %x, size: %d\n", ptr, size);
+        sizePointer sp = build_size_pointer(size, ptr);
+        int restored_size = get_size_from_size_pointer(sp);
+        void *restored_ptr = get_pointer_from_size_pointer(sp);
+        printf("restored pointer: %x, restored size: %d\n", restored_ptr, restored_size);
+    }
+    exit(0);
+    */
 }
 
 void* my_alloc(size_t size) {
-    int index = size / 16;
-    int size_normalized = ((size-1) / 16 + 1) * 16; //pow(2, index + 3);
-    if (size_normalized == 0) {
-        size_normalized = 8;
-    }
-    header *first_p = headers[index];
-#ifdef DEBUG
-    LOG("malloc was called with size %d, first_p: %x, index: %d, size_normalized: %d\n", size, first_p, index, size_normalized);
-#endif
-    //empty bucket
-    if (!first_p ) {
-#ifdef DEBUG
-        LOGT("new page ordered because of !first_p\n");
-#endif
-        char *page = get_block_from_system();
-#ifdef DEBUG
-        LOG("page: %x, page + BLOCKSIZE: %x\n", page, page + BLOCKSIZE);
-#endif
-        int package_size = size_normalized + header_size;
-        int max_offset = (BLOCKSIZE/package_size -1) * package_size;
-        char *max_package = ((size_t) page) + ((size_t) max_offset);
-        char *current_package = page;
-        header *current_header;
-        while (current_package < max_package) {
-            current_header = current_package;
-            *current_header = (header) {size_normalized, (struct header *) (current_package + package_size)};
-#ifdef DEBUG
-            LOG("address: %x, size: %d, address of next package: %x\n", current_header, current_header->size, current_header->next);
-#endif
-            current_package += package_size;
-        }
-        current_header = current_package;
-        *current_header = (header) {size_normalized, NULL};
-#ifdef DEBUG
-        LOG(" last header in page: address: %x, size: %d, address of next package: %x\n", current_header, current_header->size, current_header->next);
-#endif
-        first_p = (header *) page;
-    }
-    headers[index] = first_p->next;
-    first_p->next = NULL;
-    return first_p + 1; //+1 is implicitly converted to +header_size
+
 }
 
 void my_free(void* ptr) {
-#ifdef DEBUG
-    LOG("free was called with pointer to %x\n", ptr);
-#endif
-    header *header_of_ptr = (header *) (((char *) ptr) - header_size);
-    int index  = (header_of_ptr->size) / 16;//log2(header_of_ptr->size) - 3; //find the size of the memory that ptr points to
-#ifdef DEBUG
-    LOG("address of header_of_ptr: %x, size: %d, index: %d, address of next package: %x\n", header_of_ptr, header_of_ptr->size, index, header_of_ptr->next);
-    LOG("currently headers[%d] points to %x\n", index, headers[index]);
-#endif
-    //append header_of_pointer to the front of headers
-    header_of_ptr->next = headers[index];
-    headers[index] = header_of_ptr;
-#ifdef DEBUG
-    LOG("after free: header_of_ptr->next: %x, headers[index]: %x\n", header_of_ptr->next, headers[index]);
-#endif
+
 }
